8eed98be19af364fa34b9e179bf672633056ddcd
emanuellysilv <emanuellydasilvafurtado@gmail.com>
2024-08-21 21:52:49 -0300

    add: código de balanço trimestral

---
+++ aula01.java
@@ -0,0 +1,14 @@
+class BalancoTrimestral {
+    public static void main(String[] args) {
+        int gastosJaneiro;
+        gastosJaneiro = 15000 ;
+        int gastosFevereiro;
+        gastosFevereiro = 23000
+        int gastosMarco;
+        gastosMarco = 17000
+
+         int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+        System.out.println(gastosTrimestre);
+    }
+}    
